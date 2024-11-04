38d79cd6585d4fbbbb93c1f2e3a8b6ebb141278d
alok.rahul <alok.rahul.devops@gmail.com>
2024-11-05 00:06:38 +0100

    Record Keyboard and mouse events and call main function

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,10 +2,22 @@
 import sys
 import pygame
 
-class AlienInvasion:
+class FightAlienInvasion:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Fight Alien Invasion")
 
     def run_game(self):
+        #Start the main loop for  the game
+        while True:
+            #This is to watch keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    fai = FightAlienInvasion()
+    fai.run_game()
